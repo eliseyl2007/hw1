9580fd7ee501a0a840a11fccfeb433f76d8aaa5a
Elisei <elisey-l@mail.ru>
2023-09-09 21:57:16 +0300

    0006

---
+++ task06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ int remDiv = number % 2; 
+if (remDiv == 0 )
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

d7ece55405b6f7c37eabb523d9842a58baa2a2ba
Elisei <elisey-l@mail.ru>
2023-09-09 21:55:57 +0300

    0001

---
+++ task02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 >= number2)
+{
+    Console.WriteLine("первое число большее");
+    Console.WriteLine("второе число меньшие");
+}
+else
+{
+    Console.WriteLine("второе число большее");
+    Console.WriteLine("первое число меньшие");
+}

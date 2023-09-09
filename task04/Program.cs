21b97cf7188e450788964852b9ab061ace331f48
Elisei <elisey-l@mail.ru>
2023-09-09 21:56:31 +0300

    0004

---
+++ task04/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (number1 >= number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+if (max >= number3)
+{
+    max = max;
+}
+else
+{
+    max = number3;
+}
+Console.WriteLine($"Максимальное число {max}");
